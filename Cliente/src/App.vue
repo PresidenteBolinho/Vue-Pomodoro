9daf1af1df1668693eb3b5e08d7f601850eb92da
Gustavo Silva <gustavospr@hotmail.com>
2021-11-19 18:34:28 -0300

    Estilização da página

--- Cliente/src/App.vue
+++ Cliente/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div id="App">
-    <router-view></router-view>
+  <div id="app" :class="modo">
+    <router-view :modo="modo"></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App",
+  name: "app",
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap");
 
+* {
+  font-family: "Inter", sans-serif;
+  color: #2c3e50;
+}
 </style>
